7b621c7de5191980eb1f5480ba2007fe8ef84bf0
DogGod456 <Alexviuschin@yandex.ru>
2024-12-19 15:00:40 +0300

    refactor: изменен порядок функций, принадлежащх к HandleNoteByID, в коде

--- handlers/notes.go
+++ handlers/notes.go
@@ -2,13 +2,12 @@ package handlers
 
 import (
 	"encoding/json"
-	"log"
 	"net/http"
 	"strconv"
 )
 
+// HandleNotes обработчик методов
 func HandleNotes(w http.ResponseWriter, r *http.Request) {
-
 	switch r.Method {
 	case http.MethodPost:
 		CreateNote(w, r)
@@ -20,15 +19,17 @@ func HandleNotes(w http.ResponseWriter, r *http.Request) {
 
 }
 
+// GetAllNotes получение всех данных о заметках пользователя
 func GetAllNotes(w http.ResponseWriter, r *http.Request) {
 	session, _ := store.Get(r, "session-name")
-	userID := session.Values["user_id"] // Извлекаем ID пользователя из сессии
+	userID := session.Values["user_id"]
 
 	rows, err := db.Query("SELECT id, user_id, title, content FROM notes WHERE user_id = $1", userID)
 	if err != nil {
 		http.Error(w, "Ошибка получения данных", http.StatusInternalServerError)
 		return
 	}
+
 	defer rows.Close()
 
 	var notes []Note
@@ -39,11 +40,10 @@ func GetAllNotes(w http.ResponseWriter, r *http.Request) {
 			http.Error(w, "Ошибка чтения данных заметки", http.StatusInternalServerError)
 			return
 		}
-		notes = append(notes, note) // Добавляем заметку в список заметок пользователя
+		notes = append(notes, note)
 	}
 
 	jsonResponse, err := json.Marshal(notes)
-	log.Println(jsonResponse)
 	if err != nil {
 		http.Error(w, "Ошибка формирования ответа", http.StatusInternalServerError)
 		return
@@ -53,26 +53,20 @@ func GetAllNotes(w http.ResponseWriter, r *http.Request) {
 	w.Write(jsonResponse)
 }
 
-// Обработчик для создания новой заметки (AJAX)
+// CreateNote Обработчик для создания новой заметки
 func CreateNote(w http.ResponseWriter, r *http.Request) {
 	var note Note
 	session, _ := store.Get(r, "session-name")
-	userID := session.Values["user_id"]
-	note.UserID = userID.(int)
-	log.Println(userID, store, session)
+	note.UserID = session.Values["user_id"].(int)
+
 	// Декодируем JSON из тела запроса
 	if err := json.NewDecoder(r.Body).Decode(&note); err != nil {
 		http.Error(w, "Ошибка декодирования данных", http.StatusBadRequest)
 		return
 	}
 
-	//session, _ := store.Get(r, "session-name")
-	//userID := session.Values["user_id"]
-	//note.UserID = session.Values["user_id"].(int) // Извлекаем ID пользователя из сессии
-
 	err := db.QueryRow("INSERT INTO notes (user_id,title ,content ) VALUES ($1,$2,$3) RETURNING id",
 		note.UserID, note.Title, note.Content).Scan(&note.ID)
-
 	if err != nil {
 		http.Error(w, "Ошибка создания заметки", http.StatusInternalServerError)
 		return
@@ -88,11 +82,11 @@ func CreateNote(w http.ResponseWriter, r *http.Request) {
 	w.Write(jsonResponse)
 }
 
+// HandleNoteByID обработчик методов для одной заметки (по ID)
 func HandleNoteByID(w http.ResponseWriter, r *http.Request) {
-
 	idNoteStr := r.URL.Path[len("/notes/"):]
-	log.Println(idNoteStr)
 	idNote, err := strconv.Atoi(idNoteStr)
+
 	if err != nil {
 		http.Error(w, "Ошибка конвертации ID ", http.StatusBadRequest)
 		return
@@ -109,54 +103,14 @@ func HandleNoteByID(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func DeleteNoteByID(w http.ResponseWriter, r *http.Request, id int) {
-	session, _ := store.Get(r, "session-name")
-	userID := session.Values["user_id"]
-
-	rows, err := db.Query("SELECT id, user_id, title, content FROM notes WHERE id = $1", id)
-	if err != nil {
-		http.Error(w, "Ошибка получения данных1", http.StatusInternalServerError)
-		return
-	}
-
-	defer rows.Close()
-
-	if !rows.Next() {
-		return
-	}
-
-	var note Note
-	if err := rows.Scan(&note.ID, &note.UserID, &note.Title, &note.Content); err != nil {
-		http.Error(w, "Ошибка получения данных2", http.StatusInternalServerError)
-		return
-	}
-
-	if note.UserID != userID {
-		http.Error(w, "Пошел нахуй, пидр", http.StatusForbidden)
-		return
-	}
-
-	_, err = db.Exec("DELETE FROM notes WHERE id = $1", id)
-
-	jsonResponse, err := json.Marshal(note)
-	log.Println(jsonResponse)
-	if err != nil {
-		http.Error(w, "Ошибка формирования ответа", http.StatusInternalServerError)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-
-	w.Write(jsonResponse)
-}
-
+// GetNoteByID получение всей информации конкретной заметки
 func GetNoteByID(w http.ResponseWriter, r *http.Request, id int) {
 	session, _ := store.Get(r, "session-name")
 	userID := session.Values["user_id"]
 
 	rows, err := db.Query("SELECT id, user_id, title, content FROM notes WHERE id = $1 and user_id = $2", id, userID)
 	if err != nil {
-		http.Error(w, "Ошибка получения данных2", http.StatusInternalServerError)
+		http.Error(w, "Ошибка получения данных", http.StatusInternalServerError)
 		return
 	}
 
@@ -172,8 +126,8 @@ func GetNoteByID(w http.ResponseWriter, r *http.Request, id int) {
 		http.Error(w, "Ошибка чтения данных заметки", http.StatusInternalServerError)
 		return
 	}
+
 	jsonResponse, err := json.Marshal(note)
-	log.Println(jsonResponse)
 	if err != nil {
 		http.Error(w, "Ошибка формирования ответа", http.StatusInternalServerError)
 		return
@@ -184,20 +138,63 @@ func GetNoteByID(w http.ResponseWriter, r *http.Request, id int) {
 
 }
 
-// Обработчик для обновления существующей заметки
+// UpdateNoteById Обработчик для обновления существующей заметки
 func UpdateNoteById(w http.ResponseWriter, r *http.Request, id int) {
-
 	var note Note
-
 	if err := json.NewDecoder(r.Body).Decode(&note); err != nil {
 		http.Error(w, "Ошибка декодирования данных ", http.StatusBadRequest)
 		return
 	}
 
-	note.ID = id // Устанавливаем ID для обновления
-	log.Println("qwe", note.ID)
-	if _, err := db.Exec("UPDATE notes SET content=$1 WHERE id=$2", note.Content, id); err != nil {
+	note.ID = id
+
+	_, err := db.Exec("UPDATE notes SET content=$1 WHERE id=$2", note.Content, id)
+	if err != nil {
 		http.Error(w, "Ошибка обновления заметки ", http.StatusInternalServerError)
 		return
 	}
+}
+
+// DeleteNoteByID удаление конкретной заметки
+func DeleteNoteByID(w http.ResponseWriter, r *http.Request, id int) {
+	session, _ := store.Get(r, "session-name")
+	userID := session.Values["user_id"]
+
+	rows, err := db.Query("SELECT id, user_id, title, content FROM notes WHERE id = $1", id)
+	if err != nil {
+		http.Error(w, "Ошибка получения данных", http.StatusInternalServerError)
+		return
+	}
+
+	defer rows.Close()
+
+	if !rows.Next() {
+		return
+	}
+
+	var note Note
+	if err := rows.Scan(&note.ID, &note.UserID, &note.Title, &note.Content); err != nil {
+		http.Error(w, "Ошибка чтения данных", http.StatusInternalServerError)
+		return
+	}
+
+	if note.UserID != userID {
+		http.Error(w, "Ошибка доступа", http.StatusForbidden)
+		return
+	}
+
+	_, err = db.Exec("DELETE FROM notes WHERE id = $1", id)
+	if err != nil {
+		http.Error(w, "Ошибка удаления записи", http.StatusInternalServerError)
+		return
+	}
+
+	jsonResponse, err := json.Marshal(note)
+	if err != nil {
+		http.Error(w, "Ошибка формирования ответа", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jsonResponse)
 }
